4d8f9698e1b7294c91eaeabfd06152e21f17c068
EslamAtef <eslam.atif93@gmail.com>
2020-01-16 21:29:51 +0200

    Add Memeber Details

--- Dating-SPA/src/app/_services/auth.service.ts
+++ Dating-SPA/src/app/_services/auth.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  baseUrl = 'http://localhost:5000/api/auth/';
+  baseUrl = environment.apiUrl + 'auth/';
   jwtHelper = new JwtHelperService();
   decodedToken: any;
 
@@ -17,17 +18,17 @@ export class AuthService {
     return this.http.post(this.baseUrl + 'login', model).pipe(
       map((response: any) => {
         const user = response;
-        if(user) {
+        if (user) {
           localStorage.setItem('token', user.token);
-          //this.decodedToken = this.jwtHelper.decodeToken(user.token);
-          //console.log(this.decodedToken);
+          // this.decodedToken = this.jwtHelper.decodeToken(user.token);
+          // console.log(this.decodedToken);
         }
       })
     );
   }
 
   register(model: any) {
-    return this.http.post(this.baseUrl + 'register', model)
+    return this.http.post(this.baseUrl + 'register', model);
   }
 
   loggedIn() {
